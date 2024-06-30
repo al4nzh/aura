c30dbb916e619c4362c681f54ebe103aec349a4b
Alan Zhainar <alanzhainar05@mail.ru>
2024-06-30 20:33:48 +0500

    initialized db and models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField
+    image = models.ImageField(upload_to="images/")
+    aura = models.FloatField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
 # Create your models here.
